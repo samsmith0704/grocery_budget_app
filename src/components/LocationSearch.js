679f1ba8434722aacd12286bf08b747d344ee41c
Sam Smith <sjs5pg@virginia.edu>
2022-02-19 16:55:01 -0500

    puts data into a list

--- src/components/LocationSearch.js
+++ src/components/LocationSearch.js
@@ -5,7 +5,7 @@ import React from "react";
 import { useState } from "react";
 
 const accessToken =
-  "eyJhbGciOiJSUzI1NiIsImprdSI6Imh0dHBzOi8vYXBpLmtyb2dlci5jb20vdjEvLndlbGwta25vd24vandrcy5qc29uIiwia2lkIjoiWjRGZDNtc2tJSDg4aXJ0N0xCNWM2Zz09IiwidHlwIjoiSldUIn0.eyJhdWQiOiJidWRnZXRhcHAyLTEwNzg0ZTk2YjM0YmIxMWIxMGIxNWYzMWZhNDM4M2EwNDYyNzYyMjM0MjEwMzEwODQ0NiIsImV4cCI6MTY0NDg2MTQyNCwiaWF0IjoxNjQ0ODU5NjE5LCJpc3MiOiJhcGkua3JvZ2VyLmNvbSIsInN1YiI6IjkyNjAxZDY0LTI2N2UtNTk5Ny1iMjRmLTVkZjM2MTM1NmM5NiIsInNjb3BlIjoicHJvZHVjdC5jb21wYWN0IiwiYXV0aEF0IjoxNjQ0ODU5NjI0NTEzNjgwMzg5LCJhenAiOiJidWRnZXRhcHAyLTEwNzg0ZTk2YjM0YmIxMWIxMGIxNWYzMWZhNDM4M2EwNDYyNzYyMjM0MjEwMzEwODQ0NiJ9.iYkgygYuKc2qhrNGBdaOcls5NZ8E09FVPXGsR06zLlxJbjziQjfnjhzP70NU0lvYbe0wE7Tt9WcpL5elQ75XzR2TMeicjJHBCPARSgL8Dcpl2Hq3Itct652Bi0fZnyWurXdnAQ5QR5pbrxAW_Mw-rdkYWvCnqSj0Fy2viar8rD8BdQbhNJ6Y0YQ9rnYdXmWkPyaTgahG7Y3FwOjmUQoTLerEc2E0VRXGL6acXUiKsEh8ry-Y22zm88hJqyS3TAXag59PbU0JhdjPcWW8Gz3sLxzj_KAj8HUJmMdpo0-PwNcr2X1mjfNFYCZ_uoiGk3G9ww5Ff6B9357c2-v2RIWBiA";
+  "eyJhbGciOiJSUzI1NiIsImprdSI6Imh0dHBzOi8vYXBpLmtyb2dlci5jb20vdjEvLndlbGwta25vd24vandrcy5qc29uIiwia2lkIjoiWjRGZDNtc2tJSDg4aXJ0N0xCNWM2Zz09IiwidHlwIjoiSldUIn0.eyJhdWQiOiJidWRnZXRhcHAyLTEwNzg0ZTk2YjM0YmIxMWIxMGIxNWYzMWZhNDM4M2EwNDYyNzYyMjM0MjEwMzEwODQ0NiIsImV4cCI6MTY0NTMwODc2NywiaWF0IjoxNjQ1MzA2OTYyLCJpc3MiOiJhcGkua3JvZ2VyLmNvbSIsInN1YiI6IjkyNjAxZDY0LTI2N2UtNTk5Ny1iMjRmLTVkZjM2MTM1NmM5NiIsInNjb3BlIjoicHJvZHVjdC5jb21wYWN0IiwiYXV0aEF0IjoxNjQ1MzA2OTY3NzYxMTI0ODMyLCJhenAiOiJidWRnZXRhcHAyLTEwNzg0ZTk2YjM0YmIxMWIxMGIxNWYzMWZhNDM4M2EwNDYyNzYyMjM0MjEwMzEwODQ0NiJ9.ISm1OxFWj0W-rFGU1MHNpiwx3V1kHmJxbVj1Jd047Xuvq0WpfYcZirFPjOifHBX1TJDjQ2PnlN1ZRdm0Vthy2PvWU6Fc7TOOmVMTemMM66GocwUlDonXoogiokb2Fy8SY8RuL7ieKxfMNxWd3414BT1ytJ0Vbm1-Gyd5ZDGvAZG2RmGd65BAM7uj_N9VOnRkCcs7KaqCht4iL00d1QXMcDBp5JmJf0cX99eGVxWi_Q_uJuVpygRvMQBeWQCoJ29LcG9ce5Fj7q1aKo3HibmbiXe4U-0xMiP9sqFfIVlxlnxZl18bxx6awzUIkilQeXc1MHbpEVBSIyvQtXMhUTJyMg";
 const headers = {
   Authorization: `Bearer ${accessToken}`,
   Accept: "application/json",
@@ -25,21 +25,28 @@ const searchStoreByZip = async (zip_code) => {
 };
 
 const LocationSearch = () => {
-  const [output, setOutput] = useState("");
+  const [output, setOutput] = useState([]);
 
   const onSubmit = async () => {
     const searchTerm = document.getElementById("myInput").value;
     let result = await searchStoreByZip(searchTerm);
-    console.log(result);
-    //TODO: iterate through all the stores that were returned and just output their addresses
-    setOutput(JSON.stringify(result.data[0]));
+    const newOutput = [];
+    console.log(result.data);
+    for (let i = 0; i < result.data.length; i++) {
+      newOutput.push(result.data[i]);
+    }
+
+    setOutput(newOutput);
   };
   return (
     <div>
       <input id="myInput" type="text" />
       <input type="submit" value="Submit" onClick={onSubmit} />
       <br />
-      <h2>{output}</h2>
+
+      {/* {output.map((element) => {
+        return <h3>{element.address}</h3>;
+      })} */}
     </div>
   );
 };
