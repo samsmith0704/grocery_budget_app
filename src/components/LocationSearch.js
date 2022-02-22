877caded754267168d8ec834f405f81ea16f658d
Sam Smith <sjs5pg@virginia.edu>
2022-02-22 12:02:29 -0500

    now shows addresses

--- src/components/LocationSearch.js
+++ src/components/LocationSearch.js
@@ -5,7 +5,7 @@ import React from "react";
 import { useState } from "react";
 
 const accessToken =
-  "eyJhbGciOiJSUzI1NiIsImprdSI6Imh0dHBzOi8vYXBpLmtyb2dlci5jb20vdjEvLndlbGwta25vd24vandrcy5qc29uIiwia2lkIjoiWjRGZDNtc2tJSDg4aXJ0N0xCNWM2Zz09IiwidHlwIjoiSldUIn0.eyJhdWQiOiJidWRnZXRhcHAyLTEwNzg0ZTk2YjM0YmIxMWIxMGIxNWYzMWZhNDM4M2EwNDYyNzYyMjM0MjEwMzEwODQ0NiIsImV4cCI6MTY0NTMwODc2NywiaWF0IjoxNjQ1MzA2OTYyLCJpc3MiOiJhcGkua3JvZ2VyLmNvbSIsInN1YiI6IjkyNjAxZDY0LTI2N2UtNTk5Ny1iMjRmLTVkZjM2MTM1NmM5NiIsInNjb3BlIjoicHJvZHVjdC5jb21wYWN0IiwiYXV0aEF0IjoxNjQ1MzA2OTY3NzYxMTI0ODMyLCJhenAiOiJidWRnZXRhcHAyLTEwNzg0ZTk2YjM0YmIxMWIxMGIxNWYzMWZhNDM4M2EwNDYyNzYyMjM0MjEwMzEwODQ0NiJ9.ISm1OxFWj0W-rFGU1MHNpiwx3V1kHmJxbVj1Jd047Xuvq0WpfYcZirFPjOifHBX1TJDjQ2PnlN1ZRdm0Vthy2PvWU6Fc7TOOmVMTemMM66GocwUlDonXoogiokb2Fy8SY8RuL7ieKxfMNxWd3414BT1ytJ0Vbm1-Gyd5ZDGvAZG2RmGd65BAM7uj_N9VOnRkCcs7KaqCht4iL00d1QXMcDBp5JmJf0cX99eGVxWi_Q_uJuVpygRvMQBeWQCoJ29LcG9ce5Fj7q1aKo3HibmbiXe4U-0xMiP9sqFfIVlxlnxZl18bxx6awzUIkilQeXc1MHbpEVBSIyvQtXMhUTJyMg";
+  "eyJhbGciOiJSUzI1NiIsImprdSI6Imh0dHBzOi8vYXBpLmtyb2dlci5jb20vdjEvLndlbGwta25vd24vandrcy5qc29uIiwia2lkIjoiWjRGZDNtc2tJSDg4aXJ0N0xCNWM2Zz09IiwidHlwIjoiSldUIn0.eyJhdWQiOiJidWRnZXRhcHAyLTEwNzg0ZTk2YjM0YmIxMWIxMGIxNWYzMWZhNDM4M2EwNDYyNzYyMjM0MjEwMzEwODQ0NiIsImV4cCI6MTY0NTU1MDQwNiwiaWF0IjoxNjQ1NTQ4NjAxLCJpc3MiOiJhcGkua3JvZ2VyLmNvbSIsInN1YiI6IjkyNjAxZDY0LTI2N2UtNTk5Ny1iMjRmLTVkZjM2MTM1NmM5NiIsInNjb3BlIjoicHJvZHVjdC5jb21wYWN0IiwiYXV0aEF0IjoxNjQ1NTQ4NjA2OTMzMTgzMDAxLCJhenAiOiJidWRnZXRhcHAyLTEwNzg0ZTk2YjM0YmIxMWIxMGIxNWYzMWZhNDM4M2EwNDYyNzYyMjM0MjEwMzEwODQ0NiJ9.fzgAprKyYugvefBZJeaGLSoeYdSODYxtuET22VRGtcnyHSQviV0Jyii-lbyW7c0zrjVXlt26NXtqBxSR5lvcguAOsgz3SMveoVU9FBQo5IqB0JGmyG_78l2w8uahPGaNa21nawyvzPytNfNMeAVtx7U0DFiPArRh16uvwooAgHdroDNyh-CqGuLfDeZaznayUPKAOtdPbRtEAVp9iK8NfpQXzLiuSwsrAXx8RCjeLatOHS9WjJC41RMn-awxB2SZALPZ9WgcE7qFfg2iBJi6-d_8dc0aNp9ZZXFcqF316Gbll3tYqp8xnDkyZBzztt4pW5qdVtT5RY3oa8A3AEJ62g";
 const headers = {
   Authorization: `Bearer ${accessToken}`,
   Accept: "application/json",
@@ -33,20 +33,24 @@ const LocationSearch = () => {
     const newOutput = [];
     console.log(result.data);
     for (let i = 0; i < result.data.length; i++) {
-      newOutput.push(result.data[i]);
+      newOutput.push(result.data[i].address.addressLine1);
     }
 
     setOutput(newOutput);
+    newOutput.forEach((element) => {
+      console.log(element.address.addressLine1);
+    });
   };
   return (
     <div>
       <input id="myInput" type="text" />
       <input type="submit" value="Submit" onClick={onSubmit} />
       <br />
-
-      {/* {output.map((element) => {
-        return <h3>{element.address}</h3>;
-      })} */}
+      <ul>
+        {output.map((element) => {
+          return <li>{element}</li>;
+        })}
+      </ul>
     </div>
   );
 };
